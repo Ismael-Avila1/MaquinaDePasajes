0a61a75d9bc0bede6d76b0a9239b5e567505e4f1
Ismael Avila <ismael.guada234015@gmail.com>
2022-05-16 02:10:21 -0500

    Se agregaron comentarios al código

--- Form1.cs
+++ Form1.cs
@@ -10,27 +10,27 @@ namespace MaquinaDePasajes
         {
             InitializeComponent();
 
-            price = 9.5f;
-            amount = 0;
+            price = 9.5f;   // precio del pasaje
+            amount = 0;     // acumulado
         }
 
-        private void buttonDiezPesos_Click(object sender, EventArgs e)
+        private void buttonDiezPesos_Click(object sender, EventArgs e)  // se ejecuta cuando se presionan los 10 pesos
         {
-            amount += 10;
+            amount += 10;   // al monto se le aumentan 10 pesos
 
-            if(amount > price) {
-                amount = amount - price;
+            if(amount > price) {           // si acumulado es mayor que el monto
+                amount = amount - price;    // se le resta al acumulado el precio, para mostra el "cambio"
                 label.Text = amount.ToString();
             }
 
-            if(amount == price) {
-                Task.Delay(1000).Wait();
-                amount = 0;
-                label.Text = "--";
+            if(amount == price) {           // si son justos los 9.50
+                Task.Delay(1000).Wait();    // se hace un delay de 1s
+                amount = 0;                 // se hace cero el acumulado
+                label.Text = "--";          // se reinicia el display
             }
         }
 
-        private void buttonCincoPesos_Click(object sender, EventArgs e)
+        private void buttonCincoPesos_Click(object sender, EventArgs e) // se ejecuta cuando se presionan los 5 pesos
         {
             amount += 5;
 
@@ -48,7 +48,7 @@ namespace MaquinaDePasajes
             }
         }
 
-        private void buttonDosPesos_Click(object sender, EventArgs e)
+        private void buttonDosPesos_Click(object sender, EventArgs e)   // se ejecuta cuando se presionan los 2 pesos
         {
             amount += 2;
 
@@ -66,7 +66,7 @@ namespace MaquinaDePasajes
             }
         }
 
-        private void buttonUnPeso_Click(object sender, EventArgs e)
+        private void buttonUnPeso_Click(object sender, EventArgs e) // se ejecuta cuando se presiona el peso
         {
             amount += 1;
 
@@ -84,7 +84,7 @@ namespace MaquinaDePasajes
             }
         }
 
-        private void buttonCincuentaCentavos_Click(object sender, EventArgs e)
+        private void buttonCincuentaCentavos_Click(object sender, EventArgs e)  // se ejecuta cuando se presionan los 50 centavos
         {
             amount += .5f;
 
