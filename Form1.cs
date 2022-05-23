4e8d11cafa5c44fcd54f0d9aaee9f99468fa307b
Ismael Avila <ismael.guada234015@gmail.com>
2022-05-23 16:27:14 -0500

    Se cambió el precio del pasaje. Si el monto ingresado supera el precio del pasaje, este se resetea. Ya no guarda el cambio.

--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,7 @@ namespace MaquinaDePasajes
         {
             InitializeComponent();
 
-            price = 9.5f;   // precio del pasaje
+            price = 10;   // precio del pasaje
             amount = 0;     // acumulado
         }
 
@@ -18,13 +18,9 @@ namespace MaquinaDePasajes
         {
             amount += 10;   // al monto se le aumentan 10 pesos
 
-            if(amount > price) {           // si acumulado es mayor que el monto
-                amount = amount - price;    // se le resta al acumulado el precio, para mostra el "cambio"
+            if(amount >= price) {           // si acumulado es mayor que el monto
                 label.Text = amount.ToString();
-            }
-
-            if(amount == price) {           // si son justos los 9.50
-                Task.Delay(1000).Wait();    // se hace un delay de 1s
+                Task.Delay(300).Wait();    // se hace un delay de 1s
                 amount = 0;                 // se hace cero el acumulado
                 label.Text = "--";          // se reinicia el display
             }
@@ -34,54 +30,42 @@ namespace MaquinaDePasajes
         {
             amount += 5;
 
-            if(amount > price) {
-                amount = amount - price;
+            if(amount >= price) {
                 label.Text = amount.ToString();
+                Task.Delay(300).Wait();
+                amount = 0;
+                label.Text = "--";   
             }
             else
                 label.Text = amount.ToString();
-
-            if(amount == price) {
-                Task.Delay(1000).Wait();
-                amount = 0;
-                label.Text = "--";
-            }
         }
 
         private void buttonDosPesos_Click(object sender, EventArgs e)   // se ejecuta cuando se presionan los 2 pesos
         {
             amount += 2;
 
-            if(amount > price) {
-                amount = amount - price;
+            if(amount >= price) {
                 label.Text = amount.ToString();
-            }
-            else
-                label.Text = amount.ToString();
-
-            if(amount == price) {
-                Task.Delay(1000).Wait();
+                Task.Delay(300).Wait();
                 amount = 0;
                 label.Text = "--";
             }
+            else
+                label.Text = amount.ToString();
         }
 
         private void buttonUnPeso_Click(object sender, EventArgs e) // se ejecuta cuando se presiona el peso
         {
             amount += 1;
 
-            if(amount > price) {
-                amount = amount - price;
+            if(amount >= price) {
                 label.Text = amount.ToString();
-            }
-            else
-                label.Text = amount.ToString();
-
-            if(amount == price) {
-                Task.Delay(1000).Wait();
+                Task.Delay(300).Wait();
                 amount = 0;
                 label.Text = "--";
             }
+            else
+                label.Text = amount.ToString();
         }
 
         private void buttonCincuentaCentavos_Click(object sender, EventArgs e)  // se ejecuta cuando se presionan los 50 centavos
