03d7db7a572ebc8c1576f31c81b86309902c061a
Ismael Avila <ismael.guada234015@gmail.com>
2022-07-11 17:59:25 -0500

    Se agregó un sonido cuando se completa el monto de un pasaje.

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Media;
+
 namespace MaquinaDePasajes
 {
     public partial class Form1 : Form
@@ -7,6 +9,8 @@ namespace MaquinaDePasajes
         float amount;
         int estado;
 
+        SoundPlayer sound;
+
         public Form1()
         {
             InitializeComponent();
@@ -14,19 +18,22 @@ namespace MaquinaDePasajes
             price = 10;   // precio del pasaje
             amount = 0;     // acumulado;
             estado = 0;
+
+            sound = new SoundPlayer(@"C:\Users\ismae\Desktop\Practicas22A\TeoriaDeLaComputacion\ProyectoFinal\SistemaReal\MaquinaDePasajes\sounds\correct-ding.wav");
         }
 
         private void buttonDiezPesos_Click(object sender, EventArgs e)  // se ejecuta cuando se presionan los 10 pesos
         {
             if(estado == 0) {
                 textBoxEstados.Text = "";
-                estado = 1;
+                estado = 15;
                 textBoxEstados.Text += "Q" + estado;
             }
 
             amount += 10;   // al monto se le aumentan 10 pesos
 
             if(amount >= price) {           // si acumulado es mayor que el monto
+                sound.Play();
                 label.Text = amount.ToString();
                 Task.Delay(800).Wait();    // se hace un delay de 1s
                 amount = 0;                 // se hace cero el acumulado
@@ -39,21 +46,39 @@ namespace MaquinaDePasajes
         {
             if(estado == 0) {
                 textBoxEstados.Text = "";
-                estado = 4;
+                estado = 14;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+            else if(estado == 1) {
+                estado = 14;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+            else if(estado == 2) {
+                estado = 14;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 3) {
-                estado = 4;
+                estado = 14;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 4) {
-                estado = 1;
-                textBoxEstados.Text += "Q" + estado;
+                estado = 14;
+                textBoxEstados.Text += "Q" + estado + ", ";
             }
+            else if (estado == 10) {
+                estado = 14;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+            else if(estado == 14) {
+                estado = 15;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+
 
             amount += 5;
 
             if(amount >= price) {
+                sound.Play();
                 label.Text = amount.ToString();
                 Task.Delay(800).Wait();
                 amount = 0;
@@ -68,42 +93,63 @@ namespace MaquinaDePasajes
         {
             if(estado == 0) {
                 textBoxEstados.Text = "";
-                estado = 3;
+                estado = 10;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+            else if(estado == 1) {
+                estado = 10;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 2) {
-                estado = 3;
+                estado = 10;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 3) {
-                estado = 13;
+                estado = 11;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 4) {
-                estado = 14;
+                estado = 12;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
-            else if(estado == 9) {
+            else if(estado == 5) {
+                estado = 12;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+            else if(estado == 6) {
+                estado = 13;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+            else if(estado == 8) {
                 estado = 15;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+            else if(estado == 10) {
+                estado = 11;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+            else if(estado == 11) {
+                estado = 12;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+            else if(estado == 12) {
+                estado = 13;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 13) {
-                estado = 14;
-                textBoxEstados.Text += "Q" + estado + ", ";
-            }
-            else if(estado == 14) {
                 estado = 15;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
-            else if(estado == 15) {
-                estado = 1;
-                textBoxEstados.Text += "Q" + estado;
+            else if(estado == 14) {
+                estado = 13;
+                textBoxEstados.Text += "Q" + estado + ", ";
             }
 
 
             amount += 2;
 
             if(amount >= price) {
+                sound.Play();
                 label.Text = amount.ToString();
                 Task.Delay(800).Wait();
                 amount = 0;
@@ -118,15 +164,23 @@ namespace MaquinaDePasajes
         {
             if(estado == 0) {
                 textBoxEstados.Text = "";
+                estado = 1;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+            else if(estado == 1) {
                 estado = 2;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 2) {
-                estado = 5;
+                estado = 3;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 3) {
-                estado = 6;
+                estado = 4;
+                textBoxEstados.Text += "Q" + estado + ", ";
+            }
+            else if(estado == 4) {
+                estado = 5;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 5) {
@@ -146,30 +200,32 @@ namespace MaquinaDePasajes
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 9) {
-                estado = 10;
+                estado = 15;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 10) {
-                estado = 11;
+                estado = 3;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
             else if(estado == 11) {
-                estado = 12;
+                estado = 5;
                 textBoxEstados.Text += "Q" + estado + ", ";
             }
-            else if(estado == 12) {
-                estado = 1;
-                textBoxEstados.Text += "Q" + estado;
+            else if(estado == 14) {
+                estado = 6;
+                textBoxEstados.Text += "Q" + estado + ", ";
             }
 
 
             amount += 1;
 
             if(amount >= price) {
+                sound.Play();
                 label.Text = amount.ToString();
                 Task.Delay(800).Wait();
                 amount = 0;
                 label.Text = "--";
+                estado = 0;
             }
             else
                 label.Text = amount.ToString();
